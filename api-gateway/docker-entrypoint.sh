13e24b92025df236c16f6451a1baa7562c6d2e0f
unknown <ryan.ho.2023@scis.smu.edu.sg>
2025-11-02 19:37:33 +0800

    Initial commit

---
+++ api-gateway/docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -eu
+
+envsubst '${insurance_service_url_internal} ${billings_service_url_internal} ${events_manager_service_url_internal} ${triage_service_url_internal} ${wearable_service_url_internal} ${api_gateway_key}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+exec "$@"
