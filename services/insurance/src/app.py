c332954988e214315b1882898f0236df85e3b95b
ryan.ho.2023 <ryan.ho.2023@scis.smu.edu.sg>
2025-11-06 00:32:49 +0800

    refactor: enhance CI configuration with security testing stages and update Dockerfile for user permissions

--- services/insurance/src/app.py
+++ services/insurance/src/app.py
@@ -368,4 +368,5 @@ if __name__ == "__main__":
         print(f"Error initializing database: {str(e)}")
     
     # Start the Flask application
+    # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
     app.run(host="0.0.0.0", port=5200, debug=True)
