a696505cfb4d3f0b1655939b39fd677b647cc773
ryan.ho.2023 <ryan.ho.2023@scis.smu.edu.sg>
2025-11-05 21:58:34 +0800

    update: fix environment variable in entrypoint and enable event-stream locations in Nginx configuration

--- api-gateway/docker-entrypoint.sh
+++ api-gateway/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${insurance_service_url_internal} ${notification_service_url_internal} ${dispatch_service_url_internal} ${billings_service_url_internal} ${events_manager_service_url_internal} ${triage_service_url_internal} ${wearable_service_url_internal} ${api_gateway_key}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${insurance_service_url_internal} ${notification_service_url_internal} ${dispatch_service_url_internal} ${billings_service_url_internal} ${events_manager_service_url_internal} ${triage_service_url_internal} ${wearable_service_url_internal} ${events_stream_service_url_internal}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
